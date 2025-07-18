bcee69efaf3d436f31f5395521863540bf0398ca
Himanshu_B. <166281040+himanshubalodi2001@users.noreply.github.com>
2025-07-18 10:11:28 +0530

    Add files via upload

---
+++ Matplotlib/bigdataplot.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+for i in range(50):
+    plt.plot(np.random.rand(100), linewidth=1)
+
+plt.title("Too Much Data Can Be Confusing!")
+plt.grid(True)
+plt.tight_layout()
+plt.show()
